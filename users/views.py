0be56d37ac86cf71f6012d4950fe12429ec974c1
egboyguda <gboyguda@gmail.com>
2024-05-15 22:59:18 +0800

    added login function on views for login page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def loginForm(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('err')
+        user =authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('products')
     return render(request,'users/loginForm.html')
